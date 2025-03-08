ea59c98c258871e01120dedac865e6a8fc2dd50e
Ryan Choi <rchoi@library.ucsb.edu>
2025-03-07 16:48:47 -0800

    Fix error

--- web/themes/ucsbwebrdhsub/js/communities.js
+++ web/themes/ucsbwebrdhsub/js/communities.js
@@ -8,13 +8,13 @@
         
         const infoCircles = document.querySelectorAll(".fa-info-circle");
         infoCircles.forEach((infoCircle) => {
-            infoCircle.addEventListener("click", (e => {
+            infoCircle.addEventListener("click", e => {
                 let card = e.target.closest(".card");
                 if (card) {
-                    card.classList.toggle(".flip-card");
+                    card.classList.toggle("flip-card");
                 }
             })
-        )});
+        });
     });
 
 })(jQuery);
