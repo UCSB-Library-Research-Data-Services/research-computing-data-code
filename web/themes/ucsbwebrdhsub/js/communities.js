e7382d6c99d587548da4b27d83aeb4d351e8a29a
Ryan Choi <rchoi@library.ucsb.edu>
2025-03-07 16:26:34 -0800

    Prior update

---
+++ web/themes/ucsbwebrdhsub/js/communities.js
@@ -0,0 +1,20 @@
+/**minicalendar
+ * @file
+ * Overrride UI behaviors
+ */
+
+(function ($) {
+    $(document).ready(function() {
+        
+        const infoCircles = document.querySelectorAll(".fa-info-circle");
+        infoCircles.forEach((infoCircle) => {
+            infoCircle.addEventListener("click", (e => {
+                let card = e.closest(".card");
+                if (card) {
+                    card.classList.toggle(".flip-card");
+                }
+            })
+        )});
+    });
+
+})(jQuery);
