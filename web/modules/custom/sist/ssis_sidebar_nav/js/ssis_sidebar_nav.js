5929e80dfe4108f3d5c9ee23c43d917bace028d8
ahern112 <abraham831hernandez@gmail.com>
2025-09-16 16:07:40 -0700

    DRUPAL-184: Update js for p tag

--- web/modules/custom/sist/ssis_sidebar_nav/js/ssis_sidebar_nav.js
+++ web/modules/custom/sist/ssis_sidebar_nav/js/ssis_sidebar_nav.js
@@ -8,12 +8,12 @@
     $(document).ready(function(){
         // Check for viewport smaller than or equal to 768
         if ($(window).width() <= 768) {
-            // Get and wrap the content from the sidebar nav h2
-            var content = $('h2#block-mainnavigation-menu').html();
+            // Get and wrap the content from the sidebar nav p
+            var content = $('p#block-mainnavigation-menu').html();
             content = '<a id="sidebarnavtitlelink" data-toggle="collapse" data-parent="#accordion" aria-expanded="false" href="#collapse-sidebarnav" aria-controls="collapse-sidebarnav" class="collapsed">' + content + '</a>';
 
-            // Reinsert the content into the sidebar nav h2
-            $('h2#block-mainnavigation-menu').html( content );
+            // Reinsert the content into the sidebar nav p
+            $('p#block-mainnavigation-menu').html( content );
 
             // Add collapse attributes and classes to main sidebarnav ul
             // output for ul - <ul id="collapse-sidebarnav" class="lelando ft_sidebar_nav panel-collapse collapse" aria-labelledby="block-mainnavigation-menu">
@@ -34,7 +34,7 @@
             $("ul.left_sidebar_nav").removeAttr("aria-labelledby", "block-mainnavigation-menu");
             $("ul.left_sidebar_nav").removeClass('panel-collapse');
             $("ul.left_sidebar_nav").removeClass('collapse');
-            $("h2#block-mainnavigation-menu").find("a.sidebarnavtitlelink").contents().unwrap();
+            $("p#block-mainnavigation-menu").find("a.sidebarnavtitlelink").contents().unwrap();
 
 
         }
@@ -44,13 +44,13 @@
 
           // Check for viewport smaller than or equal to 768
           if ($(window).width() <= 768) {
-              // Get and wrap the content from the sidebar nav h2
-              var content = $('h2#block-mainnavigation-menu').html();
+              // Get and wrap the content from the sidebar nav p
+              var content = $('p#block-mainnavigation-menu').html();
               content = '<a id="sidebarnavtitlelink" data-toggle="collapse" data-parent="#accordion" aria-expanded="false" href="#collapse-sidebarnav" aria-controls="collapse-sidebarnav" class="collapsed">' + content + '</a>';
 
-              // Reinsert the content into the sidebar nav h2 only if it hasn't been placed already
+              // Reinsert the content into the sidebar nav p only if it hasn't been placed already
               if ($("a#sidebarnavtitlelink").length == 0) {
-                  $('h2#block-mainnavigation-menu').html( content );
+                  $('p#block-mainnavigation-menu').html( content );
               }
 
               // Add collapse attributes and classes to main sidebarnav ul
@@ -72,7 +72,7 @@
               $("ul.left_sidebar_nav").removeAttr("aria-labelledby", "block-mainnavigation-menu");
               $("ul.left_sidebar_nav").removeClass('panel-collapse');
               $("ul.left_sidebar_nav").removeClass('collapse');
-              $("h2#block-mainnavigation-menu").find("a.sidebarnavtitlelink").contents().unwrap();
+              $("p#block-mainnavigation-menu").find("a.sidebarnavtitlelink").contents().unwrap();
 
           }
         });
